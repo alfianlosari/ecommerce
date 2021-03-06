75106f0b9a2c4072aac6f379ec48f995585866de
Alfian Losari <alfianlosari@gmail.com>
2021-03-07 02:51:51 +0800

    Change packaging to WAR

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class SareetaApplication {
+public class SareetaApplication extends SpringBootServletInitializer {
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -20,6 +22,11 @@ public class SareetaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SareetaApplication.class);
 	}
 
 }
